4d1759b4e828375e91c401ea9263506444e8eea7
Crissxp <crissxp76@gmail.com>
2023-01-22 21:43:58 -0300

    Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/common/Button'
+import { Navbar } from '@/components/common/navbar'
 import Head from 'next/head'
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+        <Navbar />
       <Button url='/home'>
         Cargar mas peliculas
       </Button>
